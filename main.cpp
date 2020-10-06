bac017dc0eadfa668060134679c89a76de45fa39
Iago Caran <iago.caran@gmail.com>
2020-10-06 13:50:52 +0200

    Add opengl to the libraries and show basic square with it Closes issue #3

--- main.cpp
+++ main.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works with OpenGL!");
+
+    glClearColor(0, 0, 0, 0);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed) //std::cout << "Close";
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        glViewport(0, 0, window.getSize().x, window.getSize().y);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_QUADS);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(-.5f, -.5f);
+
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(.5f, -.5f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(.5f, .5f);
+
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(-.5f, .5f);
+
+        glEnd();
+
         window.display();
     }
 
