7d87eb5b3899d4a1144503efcf4c2887bca942d9
Reuh <fildadut@reuh.eu>
2020-10-09 10:43:31 +0200

    Setup OpenGL with SFML window

--- main.cpp
+++ main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // setup window
+    sf::Window window(sf::VideoMode(1280, 720), "OpenGL test");
+    window.setVerticalSyncEnabled(true);
+    window.setActive(true);
 
-    while (window.isOpen())
+    // load ressources
+    // ...
+
+    bool running = true;
+    while (running)
     {
+        // handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed) //std::cout << "Close";
-                window.close();
+            if (event.type == sf::Event::Closed)
+            {
+                running = false;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        // clear buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw
+        // ...
+
+        // swap buffers
         window.display();
     }
+
+    // release ressources
+    // ...
 
     return 0;
 }
