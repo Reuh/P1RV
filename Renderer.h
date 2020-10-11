346b25f67648371b85b75f8905dce79f2e038a7c
Iago Caran <iago.caran@gmail.com>
2020-10-11 18:41:34 +0200

    Create base class for Renderer Components

---
+++ Renderer.h
@@ -0,0 +1,18 @@
+//
+// Created by iagoc on 11/10/2020.
+//
+
+#ifndef P1RV_RENDERER_H
+#define P1RV_RENDERER_H
+
+
+#include "Component.h"
+
+class Renderer : public Component {
+public:
+    // TODO Make it static and build an interface to pass the parameters from a reference to the object
+    virtual void render() = 0;
+};
+
+
+#endif //P1RV_RENDERER_H
