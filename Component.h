ceda56b099567a2212f121d6ab9eed9cbb3561b6
Iago Caran <iago.caran@gmail.com>
2020-10-11 18:40:54 +0200

    Create base class for GameObject Components

---
+++ Component.h
@@ -0,0 +1,15 @@
+//
+// Created by iagoc on 11/10/2020.
+//
+
+#ifndef P1RV_COMPONENT_H
+#define P1RV_COMPONENT_H
+
+
+class Component {
+public:
+    virtual ~Component() = default;
+};
+
+
+#endif //P1RV_COMPONENT_H
