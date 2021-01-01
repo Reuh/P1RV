daf8ec9508a0ca2b97566720b5f76be23d99c0df
Iago Caran <iago.caran@gmail.com>
2021-01-01 15:51:18 +0100

    Create RelicScript so that it interacts with the player

---
+++ component/RelicScript.cpp
@@ -0,0 +1,33 @@
+//
+// Created by iagoc on 30/12/2020.
+//
+
+#include <iostream>
+#include <glm/glm.hpp>
+#include "RelicScript.hpp"
+
+
+RelicScript::RelicScript(GameObject * relic, GameObject * player) {
+    this->relic = relic;
+    this->player = player;
+}
+
+void RelicScript::update(float dt) {
+    auto relicPos = relic->getTransform()->getPosition();
+    //std::cout << "R " << relicPos.x << " : " << relicPos.y << " : " << relicPos.z << std::endl;
+    auto playerPos = player->getTransform()->getPosition();
+    //std::cout << "P " << playerPos.x << " : " << playerPos.y << " : " << playerPos.z << std::endl;
+    if (this->attached) {
+        // TODO : Place relic in players field of view
+        relic->getTransform()->setPosition(player->getTransform()->getPosition() + glm::vec3(.2, 0, .2));
+        // TODO : Check if relic inside target zone
+    } else {
+        auto distance = relicPos - playerPos;
+        // std::cout << glm::length(distance) << std::endl;
+        if (glm::length(distance) <= 0.7) {
+            attached = true;
+        }
+    }
+}
+
+
