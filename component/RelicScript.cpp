0bfa9c0980d9c12ecdd1db2ffaeea1a210167694
Iago Caran <iago.caran@gmail.com>
2021-01-05 17:03:12 +0100

    Update RelicScript to change relic size when attached and stay in the player's vision

--- component/RelicScript.cpp
+++ component/RelicScript.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <glm/glm.hpp>
+#include <glm/gtx/scalar_multiplication.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include "RelicScript.hpp"
 
 
@@ -18,14 +20,17 @@ void RelicScript::update(float dt) {
     auto playerPos = player->getTransform()->getPosition();
     //std::cout << "P " << playerPos.x << " : " << playerPos.y << " : " << playerPos.z << std::endl;
     if (this->attached) {
-        // TODO : Place relic in players field of view
-        relic->getTransform()->setPosition(player->getTransform()->getPosition() + glm::vec3(.2, 0, .2));
+        auto t = glm::rotateY(player->getTransform()->getFront(), glm::radians(30.f));
+        relic->getTransform()->setPosition(player->getTransform()->getPosition() + 0.25 * t + glm::vec3(0, -.1, 0));
+        // TODO : Rotate relic to follow the camera orientation
+
         // TODO : Check if relic inside target zone
     } else {
         auto distance = relicPos - playerPos;
         // std::cout << glm::length(distance) << std::endl;
         if (glm::length(distance) <= 1.1) {
             attached = true;
+            relic->getTransform()->scale(glm::vec3(0.2, 0.2, 0.2));
         }
     }
 }
