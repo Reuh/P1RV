361215282676cb32326dbb2ffa33a097a4fbc5cb
Iago Caran <iago.caran@gmail.com>
2021-01-05 22:50:54 +0100

    Setup TestScene and TestEnemyScript to illustrate NavMeshNavigator usage

--- component/TestEnemyScript.cpp
+++ component/TestEnemyScript.cpp
@@ -1,10 +1,21 @@
 #include "TestEnemyScript.hpp"
 #include "Script.hpp"
-
+#include "GameObject.hpp"
+#include "NavMeshNavigator.hpp"
 #include <iostream>
 
 void TestEnemyScript::update(float dt) {
-	// update IA and stuff
+    glm::vec3 begin = glm::vec3(-0.5, 0, -0.5);
+    glm::vec3 end = glm::vec3(-0.5, 0, -4);
+	auto enemy = this->object;
+	glm::vec3 pos = enemy->getComponent<NavMeshNavigator>()->nextStep(dt);
+    std::cout << pos.x << " : " << pos.y << " : " << pos.z << std::endl;
+	enemy->getTransform()->setPosition(pos);
+	if (enemy->getTransform()->getPosition() == begin) {
+        enemy->getComponent<NavMeshNavigator>()->setDestination(end);
+	} else if (enemy->getTransform()->getPosition() == end) {
+        enemy->getComponent<NavMeshNavigator>()->setDestination(begin);
+	}
 }
 
 void TestEnemyScript::onHit() {
--- scene/TestScene.cpp
+++ scene/TestScene.cpp
@@ -12,10 +12,33 @@
 
 #include <glm/glm.hpp>
 #include <iostream>
+#include <component/NavMesh.hpp>
+#include <component/NavMeshNavigator.hpp>
 
 using namespace glm;
 
 void TestScene::initialize() {
+    auto navmesh = new NavMesh("models/navmesh.obj");
+    glm::vec3 begin = vec3(-0.5, 0, -0.5);
+    glm::vec3 end = vec3(-0.5, 0, -4);
+    auto result = navmesh->findPath(begin, end);
+
+    cout << result.size() << " points." << endl;
+
+    auto * point = new GameObject();
+    point->addComponent(new ModelRenderer("models/cube/cube.gltf"));
+    point->getTransform()->scale(vec3(0.125, 0.125, 0.125));
+    point->getTransform()->setPosition(result[0].second);
+    point->addComponent(new NavMeshNavigator(navmesh));
+    point->addComponent(new TestEnemyScript());
+    addObject(point);
+
+    auto map = new GameObject();
+    map->getTransform()->setPosition(vec3(0, -1, 0));
+    map->addComponent(new ModelRenderer("models/navmesh.obj"));
+    addObject(map);
+
+    /*
     auto model = new GameObject();
     Transform* transform = model->getTransform();
     transform->scale(vec3(0.5,0.5,0.5));
@@ -23,18 +46,22 @@ void TestScene::initialize() {
     model->addComponent(new BoxCollider(true, vec3(-1,-1,-1), vec3(1,1,1))); // TODO: automatically calculate from model data
     model->addComponent(new TestEnemyScript());
     addObject(model);
+     */
 
+    /*
     auto maze = new GameObject();
     maze->getTransform()->scale(vec3(1, 1, 1));
     // TODO : Add a way to change model color from ModelRenderer properties
-    //maze->addComponent(new ModelRenderer("models/labyrinth.obj"));
+    // maze->addComponent(new ModelRenderer("models/labyrinth.obj"));
     maze->addComponent(new ModelRenderer("models/level/level.obj"));
     addObject(maze);
+    */
 
     auto player = new GameObject();
     player->addComponent(new PlayerScript());
     player->addComponent(new BoxCollider(true, vec3(-.2,0,-.2), vec3(.2,.7,.2)));
-    player->getTransform()->setPosition(glm::vec3(0,1,30));
+    player->getTransform()->setPosition(glm::vec3(-0.5, 0, -0.5));
+    // player->getTransform()->setPosition(glm::vec3(0,1,30));
     addObject(player);
 
     auto skybox = new GameObject();
@@ -44,10 +71,12 @@ void TestScene::initialize() {
     skybox->addComponent(skyboxModel);
     addObject(skybox);
 
+    /*
     auto relic = new GameObject();
     relic->addComponent(new ModelRenderer("models/Relic.obj"));
     relic->getTransform()->setPosition(glm::vec3(2, -0.5, 0));
     relic->getTransform()->scale(glm::vec3(0.25, 0.25, 0.25));
     relic->addComponent(new RelicScript(relic, player));
     addObject(relic);
+    */
 }
